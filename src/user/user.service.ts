2ebb2b8d786cfd1062f33e6ca66944cb6cc84116
Pavel Kizhlo <pavelkizh@gmail.com>
2023-02-05 18:19:57 +0300

    feat: implement /user endpoint functionality

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { Database } from '../database/database';
+import { NoRequiredEntity } from '../database/errors/noRequireEntity.error';
+import { InvalidPassword } from '../database/errors/invalidPassword';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private db: Database) {}
+
+  getAllUsers() {
+    return this.db.users.getAll();
+  }
+
+  getUserById(id: string) {
+    return this.db.users.getById(id);
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    return this.db.users.create(createUserDto);
+  }
+
+  updatePassword(id: string, updatePasswordDto: UpdatePasswordDto) {
+    try {
+      return this.db.users.update(id, updatePasswordDto);
+    } catch (err) {
+      throw err instanceof NoRequiredEntity
+        ? new NotFoundException('No users with such id')
+        : err instanceof InvalidPassword
+        ? new ForbiddenException('Old password is invalid')
+        : err;
+    }
+  }
+
+  removeUser(id: string) {
+    try {
+      this.db.users.remove(id);
+    } catch (err) {
+      throw err instanceof NoRequiredEntity
+        ? new NotFoundException('No users with such id')
+        : err;
+    }
+  }
+}
