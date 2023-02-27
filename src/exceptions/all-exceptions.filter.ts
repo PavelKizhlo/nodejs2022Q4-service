f60c2e18df148c78384eafe8d8327c1062605669
Pavel Kizhlo <pavelkizh@gmail.com>
2023-02-27 12:27:40 +0300

    feat: implement error handling including sending response with an appropriate http status code and errors logging

--- src/exceptions/all-exceptions.filter.ts
+++ src/exceptions/all-exceptions.filter.ts
@@ -1,5 +1,11 @@
-import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { AbstractHttpAdapter, BaseExceptionFilter } from '@nestjs/core';
+import { Request, Response } from 'express';
 import { LoggingService } from '../logging/logging.service';
 
 @Catch()
@@ -11,21 +17,45 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
     super(httpAdapter);
   }
 
-  catch(exception: HttpException, host: ArgumentsHost) {
-    super.catch(exception, host);
-
+  catch(exception: unknown, host: ArgumentsHost): void {
     this.loggingService.setContext('ExceptionFilter');
 
-    const req = host.switchToHttp().getRequest();
-    const res = host.switchToHttp().getResponse();
+    const context = host.switchToHttp();
+    const req = context.getRequest<Request>();
+    const res = context.getResponse<Response>();
     const { method, url, body, query } = req;
-    const { statusCode } = res;
     const now = Date.now();
+    let httpStatus;
 
-    this.loggingService.warn(
-      `${method} ${url} query: ${JSON.stringify(query)} body: ${JSON.stringify(
-        body,
-      )} status: ${statusCode}, responseTime: ${Date.now() - now}ms`,
-    );
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus();
+
+      this.loggingService.warn(
+        `${method} ${url} query: ${JSON.stringify(
+          query,
+        )} body: ${JSON.stringify(body)} status: ${httpStatus}, responseTime: ${
+          Date.now() - now
+        }ms`,
+      );
+    } else {
+      httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+      const reason = exception instanceof Error ? exception.message : '';
+
+      this.loggingService.error(
+        `${method} ${url} query: ${JSON.stringify(
+          query,
+        )} body: ${JSON.stringify(body)} status: ${httpStatus}, responseTime: ${
+          Date.now() - now
+        }ms${reason ? ' REASON: ' : ''}${reason}`,
+      );
+    }
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: req.url,
+    };
+
+    this.httpAdapter.reply(res, responseBody, httpStatus);
   }
 }
